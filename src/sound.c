cde346dd64e07f845d71704afc5829d4568e692c
sk-1982 <13491412+sk-1982@users.noreply.github.com>
2024-05-04 09:22:11 -0400

    audio support

--- src/sound.c
+++ src/sound.c
@@ -1,16 +1,34 @@
 #include "sound.h"
 #include <stdio.h>
+#include <SDL.h>
+#include <SDL_mixer.h>
+
 
 void play_sound(SoundID id)
 {
 	char file_path[32];
-	sprintf(file_path, "./res/sounds/%d.wav", id);
-	
+	sprintf(file_path, "/res/sounds/%d.wav", id);
+	Mix_Chunk* chunk = Mix_LoadWAV(file_path);
+	if (!chunk) {
+		printf("Failed to load audio %d\n", id);
+		return;
+	}
+
+	Mix_PlayChannel(-1, chunk, 0);
+	Mix_FreeChunk(chunk);
 }
 
 int sounds_init()
 {
+	if(SDL_Init(SDL_INIT_AUDIO) < 0) {
+		printf("Failed to initialize SDL\n");
+		return 0;
+	}
 
+	if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 1, 1024) < 0) {
+		printf("Failed to initialize audio\n");
+		return 0;
+	}
 
 	return 1;
 }
